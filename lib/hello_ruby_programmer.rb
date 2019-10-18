abebccdda7852179cf6cfe98f8c6892252ca67ed
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-18 17:36:29 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,6 +1,7 @@
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
   puts "Please enter your name so that we can greet you more personally:"
+  name = gets.strip
 end
 
 greeting(name)
